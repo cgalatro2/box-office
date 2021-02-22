e039111b4303ad1ab205fa66b30e454fdaf3a61e
Chase Galatro <chasegalatro@gmail.com>
2021-02-22 16:34:22 -0500

    router config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div>Hey</div>;
+export default function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <div>This the home page</div>
+      </Route>
+
+      <Route exact path="/starred">
+        <div>This the starred page</div>
+      </Route>
+
+      <Route>404</Route>
+    </Switch>
+  );
 }
-
-export default App;
